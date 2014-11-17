a8f5c5925d204b3489c44bc2948c62e71ddcc7dd
itboke <it_web_coder@163.com>
2014-11-17 18:25:55 +0800

    python数据类型
    python数据类型

---
+++ dataType.py
@@ -0,0 +1,13 @@
+#序列类型的数据类型：列表、元组、字符串
+
+#列表的定义:[元素1,元素2,....] 通过索引下标来引用
+
+list = [1,2,3,4,'python','helloworld']
+
+#通过列表的append(item)向列表后面添加元素
+
+print(list[4]); #输出的是python
+
+list.append('beautyful');
+
+print(list);#输出[1, 2, 3, 4, 'python', 'helloworld', 'beautyful']
